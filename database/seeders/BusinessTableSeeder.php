83656dc46a905e7db53c56e63d9977df20fd2f85
Arinze Anakor <arinzeanakor@gmail.com>
2021-06-11 22:29:13 +0100

    same commit

--- database/seeders/BusinessTableSeeder.php
+++ database/seeders/BusinessTableSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\Business;
 use App\Models\BusinessUser;
 use App\Models\Client;
+use App\Models\Invoice;
+use App\Models\InvoiceItem;
 use App\Models\User;
 use Faker\Factory;
 use Illuminate\Database\Seeder;
@@ -22,20 +24,37 @@ class BusinessTableSeeder extends Seeder
 
 
         // test user
-        $user = User::factory()->create([
-            'email' => 'test@example.com',
-            'password' => bcrypt('secret')
-        ]);
-        $businesses = Business::factory(5)->create();
-        foreach($businesses as $business) {
-            Client::factory($faker->numberBetween(5, 10))->create([
-                'business_id' => $business->id,
-            ]);
-            BusinessUser::factory()->create([
-                'user_id' => $user->id,
-                'business_id' => $business->id,
+        $email = 'test@example.com';
+        $user = User::where('email', $email)->first();
+        if(!$user) {
+            $user = User::factory()->create([
+                'email' => $email,
+                'password' => bcrypt('secret')
             ]);
         }
+        
+        $businesses = Business::factory(2)->create();
+        foreach($businesses as $business) {
+            $clients = Client::factory($faker->numberBetween(1, 2))->create([
+                'business_id' => $business->id,
+            ]);
+
+            foreach($clients as $client) {
+                BusinessUser::factory()->create([
+                    'user_id' => $user->id,
+                    'business_id' => $business->id,
+                ]);
+                Invoice::factory($faker->randomElement([1, 2]))
+                        ->has(InvoiceItem::factory()->count($faker->randomElement([1, 2])))
+                        ->create([
+                            'client_id' => $client->id,
+                            'business_id' => $business->id,
+                        ]);
+            }
+        }
+
+
+
 
         $users = User::factory()
             ->count($faker->numberBetween(1, 3))
